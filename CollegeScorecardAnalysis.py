7e1fe30ed8e8778e1253345606caa4b5b1cb0941
AlyssaSharma18 <91567337+AlyssaSharma18@users.noreply.github.com>
2024-11-18 13:27:05 -0500

    Update CollegeScorecardAnalysis.py

--- CollegeScorecardAnalysis.py
+++ CollegeScorecardAnalysis.py
@@ -24,25 +24,61 @@ newdata = newdata.rename(columns={
     'ICLEVEL': 'InstitutionLevel'
 })
 
+#Here we take out faulty values (PS) from the completer 5 year repay rate row
 newdata = newdata[newdata['Completer5yrRepayRate'] != "PS"]
 newdata['Completer5yrRepayRate'] = pd.to_numeric(newdata['Completer5yrRepayRate'], errors='coerce')
 newdata.reset_index(drop=True, inplace=True)
 
-# Sort data by 'MedianEarnings10Years' in descending order and select the top 7
-top_7_data = newdata.sort_values(by='MedianEarnings10Years', ascending=False).head(7)
+# Analysis steps
 
-# Plot: Bar chart of top 7 institutions by Median Earnings 10 Years After Graduation
+# Max median earnings after 10 years was Duke
+max_median_earnings = newdata['MedianEarnings10Years'].max()
+max_earnings_row = newdata[newdata['MedianEarnings10Years'] == max_median_earnings]
+print(max_earnings_row)
+
+# Min avg cost was Elizabeth City State University
+min_average_cost = newdata['AverageCost'].min()
+lowest_cost_row = newdata[newdata['AverageCost'] == min_average_cost]
+print(lowest_cost_row)
+
+# maximum CS percentage was Montreat College
+max_cs_percent = newdata['ComputerSciencePercentage'].max()
+max_cs_percent_row = newdata[newdata['ComputerSciencePercentage'] == max_cs_percent]
+print(max_cs_percent_row)
+
+
+# Highest 50th percentile ACT admission was Duke
+max_50act = newdata['50thPercentileACTAdmission'].max()
+max_50act_row = newdata[newdata['50thPercentileACTAdmission'] == max_50act]
+print(max_50act_row)
+
+# Max 5 year repay rate we Elon
+max_Completer5yrRepayRate = newdata['Completer5yrRepayRate'].max()
+max_Completer5yrRepayRate_row = newdata[newdata['Completer5yrRepayRate'] == max_Completer5yrRepayRate]
+print(max_Completer5yrRepayRate_row)
+
+
+#Lowest Student Faculty Ratio shared with Duke, Bennett, and UNC School of Arts
+minStudFacR = newdata['StudentFacultyRatio'].min()
+minStudFacR_row = newdata[newdata['StudentFacultyRatio'] == minStudFacR]
+print(minStudFacR_row)
+
+# Plot: Bar chart of Median Earnings 10 Years After Graduation by Institution
+# Step 1: Sort data by 'MedianEarnings10Years' in descending order
+sorted_data = newdata.sort_values(by='MedianEarnings10Years', ascending=False)
+
+# Step 2: Plot the data
 plt.figure(figsize=(12, 8))
 sns.barplot(
     x='InstitutionName',
     y='MedianEarnings10Years',
-    data=top_7_data,
+    data=sorted_data,
     palette='Blues_d'
 )
 
 # Customize the chart
 plt.xticks(rotation=90)
-plt.title('Top 7 Institutions by Median Earnings 10 Years After Graduation')
+plt.title('Median Earnings 10 Years After Graduation by Institution')
 plt.xlabel('Institution Name')
 plt.ylabel('Median Earnings 10 Years ($)')
 plt.tight_layout()
